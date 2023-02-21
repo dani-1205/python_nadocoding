f229997e8785f0fa8ea0d16965fc5f34d2e165fe
DATA danida <80170950+dani-1205@users.noreply.github.com>
2023-02-21 18:28:08 +0900

    Update practice(2.21).py
    2월 21일 강의

--- practice(2.21).py
+++ practice(2.21).py
@@ -22,18 +22,29 @@
 # print(5 > 4 > 7)
 
 
-# 숫자 처리 함수
-print(abs(-5))      # abs() -> 절대값
-print(pow(4, 2))    # pow() -> 제곱 / 4^2
-print(max(5, 12))   # max() -> 최대값
-print(min(5, 12))   # min() -> 최소값
-print(round(3.14))  # round() -> 반올림
-print(round(4.99))
+# # 숫자 처리 함수
+# print(abs(-5))      # abs() -> 절대값
+# print(pow(4, 2))    # pow() -> 제곱 / 4^2
+# print(max(5, 12))   # max() -> 최대값
+# print(min(5, 12))   # min() -> 최소값
+# print(round(3.14))  # round() -> 반올림
+# print(round(4.99))
 
-from math import *      # math 라이브러리 안에 있는 모든 것을 이용하겠다는 의미
-print(floor(4.99))      # floor() -> 내림
-print(ceil(3.14))       # ceil() -> 올림
-print(sqrt(16))         # sqrt() -> 제곱근
+# from math import *      # math 라이브러리 안에 있는 모든 것을 이용하겠다는 의미
+# print(floor(4.99))      # floor() -> 내림
+# print(ceil(3.14))       # ceil() -> 올림
+# print(sqrt(16))         # sqrt() -> 제곱근
 
 
-# 랜덤함수 39:00
+# 랜덤함수
+from random import *
+print(random())         # random() -> 0.0 이상 1.0 미만의 임의의 값 생성
+print(random() * 10)    # 0.0 이상 10.0 미만의 임의의 값 생성
+print(int(random() * 10))   # 0 이상 10 미만의 임의의 값 생성
+
+print(randrange(1, 46))     # randrange(a, b) -> a 이상 b 미만의 임의의 값 생성
+
+print(randint(1, 45))       # randint(a, b) -> a 이상 b 이하의 임의의 값 생성
+
+
+#
